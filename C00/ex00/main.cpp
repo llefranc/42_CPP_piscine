f593435e70e28999579fb342226c32c920bd5fe5
Lucas Lefrancq <lucas.lefrancq@hotmail.fr>
2020-11-28 10:36:01 +0100

    c00:ex00 termine

---
+++ C00/ex00/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucaslefrancq <lucaslefrancq@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/27 18:00:46 by lucaslefran       #+#    #+#             */
+/*   Updated: 2020/11/28 10:35:25 by lucaslefran      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+std::string	trans_upper(std::string str)
+{
+	for (int i = 0; str[i]; ++i)
+		str[i]= toupper(str[i]);
+	return (str);
+}
+
+int		main(int ac, char **av)
+{
+	(void)av;
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	for (int i = 1; i < ac; ++i)
+		std::cout << trans_upper(av[i]);
+	std::cout << std::endl;
+	return (0);
+}
