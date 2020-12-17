8bdfe2e3e5687a39ccf62b1a2dde0e5261181b15
Lucas Lefrancq <lucas.lefrancq@hotmail.fr>
2020-12-17 15:25:09 +0100

    c05ex01 finished

--- C05/README.md
+++ C05/README.md
@@ -19,7 +19,7 @@
 2. `cd` into the root directory, then `cd` into C05 and choose the exercise. Then run `make`.
 
         cd 42_CPP_piscine/C05/ex00
-        make
+        makex
 
 3.  Read the subject in order to have more information about how launching a specific exercise.
     
@@ -29,9 +29,11 @@
 - [Cours sur le C++ (FR)][3]
 - [C++ iomanip reference][4]
 - [FAQ sur le C++ (FR)][5]
+- [Cours sur les execptions (FR)][6]
 
 [1]: https://github.com/lucaslefrancq/42_CPP_piscine/blob/main/C05/cpp05.en.subject.pdf
 [2]: http://www.cplusplus.com/files/tutorial.pdf
 [3]: https://openclassrooms.com/fr/courses/1894236-programmez-avec-le-langage-c/1894377-quest-ce-que-le-c
 [4]: http://www.cplusplus.com/reference/iomanip/
 [5]: https://cpp.developpez.com/faq/cpp/
+[6]: https://cpp.developpez.com/cours/cpp/?page=page_11
--- C05/ex01/main.cpp
+++ C05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lucaslefrancq <lucaslefrancq@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/16 13:15:27 by lucaslefran       #+#    #+#             */
-/*   Updated: 2020/12/17 13:41:38 by lucaslefran      ###   ########.fr       */
+/*   Updated: 2020/12/17 15:23:49 by lucaslefran      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 #include <string>
 
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 int main()
 {
-	// ----------------------------------------------
-	// -------------- BUREAUCRAT TESTS -------------- 
-	// ----------------------------------------------
+	std::cout << "----------------------------------------------\n";
+	std::cout << "-------------- BUREAUCRAT TESTS --------------\n";
+	std::cout << "----------------------------------------------\n";
 	
 	Bureaucrat henri("Henri", 1);
 
@@ -63,9 +64,81 @@ int main()
 	}
 	
 
-	// ----------------------------------------------
-	// ----------------- FORM TESTS ----------------- 
-	// ----------------------------------------------
+	std::cout << "----------------------------------------------\n";
+	std::cout << "----------------- FORM TESTS -----------------\n";
+	std::cout << "----------------------------------------------\n";
+
+	// Succesfull creation
+	try
+	{
+		std::cout << "Creating a form with default parameters\n";
+		Form f1;
+		std::cout << "Form succesfully created\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	// Succesfull creation
+	try
+	{
+		std::cout << "Creating a form with given parameters\n";
+		Form f3("F3", 30, 20);
+		std::cout << "Form succesfully created\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	// Creation failed : exception GradeTooLow
+	try
+	{
+		std::cout << "Creating a form with wrong parameters\n";
+		Form f3("F3", 30, 180);
+		std::cout << "Form succesfully created\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+
+	Form f1;
+	Form f2(f1);
+	Form f3("F3", 30, 20);
+	
+	// Testing getters
+	std::cout << "Name:" << f3.getName() << "\n";
+	std::cout << "Is signed value:" << f3.getIsSigned() << "\n";
+	std::cout << "Grade to sign:" << f3.getGradeToSign() << "\n";
+	std::cout << "Grade to execute:" << f3.getGradeToExecute() << "\n\n";
+
+	// Testing overload operator<<
+	std::cout << f1 << f2 << f3 << "\n";
+
+
+	// beSigned fails : exception GradeTooLow
+	try
+	{
+		f3.beSigned(henri);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	// beSigned works
+	Bureaucrat jimmy("Jimmy", 1);
+	try
+	{
+		f3.beSigned(jimmy);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	
 	return (0);
 }
